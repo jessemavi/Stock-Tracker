e35dd0d99c082de3e046baa062b5d4137a3bc670
Jesse Mavi <jesse.mavi@gmail.com>
2018-08-17 18:45:29 -0500

    Updates main.go to create stocks table on connection. Removes User struct and in db table.

--- main.go
+++ main.go
@@ -10,43 +10,43 @@ import (
 
 var db *sql.DB
 
-type User struct {
-    ID int `json:"id"`
-    Username string `json:"username"`
-    Password string `json:"password"`
-}
-
 type Stock struct {
     ID int `json:"id"`
     Symbol string `json:"symbol"`
-    User int `json: "user"`
 }
 
 func init() {
     var err error
-    db, err = sql.Open("postgres", "dbname=stock_tracker sslmode=disable")
+
+    db, err = sql.Open("postgres", "dbname=stock_tracker_2 sslmode=disable")
     if err != nil {
         panic(err)
     }
+
+    _, err = db.Query(`
+        create table if not exists stocks (
+            id serial primary key,
+            symbol varchar unique not null
+        );
+    `)
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+
     fmt.Println("Connected to database")
 }
 
 func main() {
-    http.HandleFunc("/", index)
     http.HandleFunc("/stocks", getStocks)
     http.HandleFunc("/stocks/add", addStock)
     http.HandleFunc("/stocks/remove", removeStock)
     http.ListenAndServe(":8080", nil)
 }
 
-func index(w http.ResponseWriter, r * http.Request) {
-    fmt.Fprintf(w, "Stock Tracker")
-}
-
-// post request to get all stocks for a user
+// post request to get all stocks
 func getStocks(w http.ResponseWriter, r *http.Request) {
-    // get user id from post request body by decoding
-    // go through stocks and get stocks with user id
+    // db query to get stocks
         // create structs with stock info from db
     // marshal stock structs into json
     // write json as response
@@ -56,18 +56,9 @@ func getStocks(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    user := User{}
-
-    decoder := json.NewDecoder(r.Body)
-    err := decoder.Decode(&user)
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
-
     stocks := []Stock{}
 
-    rows, err := db.Query("select * from stocks where user_id = $1", user.ID)
+    rows, err := db.Query("select * from stocks")
     if err != nil {
         fmt.Println(err)
         return
@@ -76,7 +67,7 @@ func getStocks(w http.ResponseWriter, r *http.Request) {
 
     for rows.Next() {
         stock := Stock{}
-        err = rows.Scan(&stock.ID, &stock.Symbol, &stock.User)
+        err = rows.Scan(&stock.ID, &stock.Symbol)
         if err != nil {
             fmt.Println(err)
             return
@@ -117,7 +108,7 @@ func addStock(w http.ResponseWriter, r *http.Request) {
         fmt.Println(err)
         return
     }
-    _, err = db.Exec("insert into stocks (symbol, user_id) values ($1, $2)", stock.Symbol, stock.User)
+    _, err = db.Exec("insert into stocks (symbol) values ($1)", stock.Symbol)
     if err != nil {
         fmt.Println(err)
         return
@@ -130,7 +121,7 @@ func addStock(w http.ResponseWriter, r *http.Request) {
 func removeStock(w http.ResponseWriter, r *http.Request) {
     // take stock object from json request body and parse it with decoder into a stock struct
     // extract data from stock struct to delete
-    // delete from db with symbol and user id
+    // delete from db with symbol
 
     if r.Method != "DELETE" {
         http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
@@ -146,7 +137,7 @@ func removeStock(w http.ResponseWriter, r *http.Request) {
         return
     }
 
-    _, err = db.Exec("delete from stocks where symbol = $1 and user_id = $2", stock.Symbol, stock.User)
+    _, err = db.Exec("delete from stocks where symbol = $1", stock.Symbol)
     if err != nil {
         fmt.Println(err)
         return
